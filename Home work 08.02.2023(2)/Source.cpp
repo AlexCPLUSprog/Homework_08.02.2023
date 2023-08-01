0f0276b1b8d1601ddb417ddf3cd47440457b5085
Алекс <Alexandr_enid@mail.ru>
2023-08-01 20:48:12 +0500

    Добавьте файлы проекта.

---
+++ Home work 08.02.2023(2)/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <windows.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include "Printer.h"
+#include "Ivehicle.h"
+#include "Car.h"
+#include "Bike.h"
+#include "Bike2.h"
+#include <memory>
+
+
+
+int main() {
+	
+	std::shared_ptr<Ivehicle> car = std::make_shared<Car>(4);
+	std::shared_ptr<Ivehicle> bike = std::make_shared<Bike>(4);
+	std::shared_ptr<Ivehicle> bike2 = std::make_shared<Bike2>(4);
+	Printer f;
+	f.getIvehicle(car);
+	f.getIvehicle(bike);
+	f.getIvehicle(bike2);
+	f.printavto();
+
+
+
+}
